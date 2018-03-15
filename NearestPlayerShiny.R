938ee48c8f32dda24d9472292581a5a6017733ef
Kevin <friggystudios@gmail.com>
2018-03-15 15:41:47 +0000

    backup

--- NearestPlayerShiny.R
+++ NearestPlayerShiny.R
@@ -60,11 +60,25 @@ ui <- fluidPage(
     
     # Output
     mainPanel(
-      conditionalPanel( condition = "!input.findMe && input.render",
-        dataTableOutput(outputId = "tblSearch")
-      ),
-      conditionalPanel( condition = "input.findMe && input.render",
-        dataTableOutput(outputId = "tblFindMe")
+      tabsetPanel(type = "tabs",
+                  tabPanel("Table",
+        conditionalPanel( condition = "!input.findMe && input.render",
+          dataTableOutput(outputId = "tblSearch")
+        ),
+        conditionalPanel( condition = "input.findMe && input.render",
+          dataTableOutput(outputId = "tblFindMe")
+        )),
+                tabPanel("Radar",
+        conditionalPanel( condition = "!input.findMe && input.render",
+          plotOutput('radarSearch'),
+          uiOutput(outputId = "radarSearchCompare")
+        ),
+        conditionalPanel( condition = "input.findMe && input.render",
+          plotOutput('radarFindMe'),
+          uiOutput(outputId = "radarMeCompare")
+        )
+        
+        )
       )
     )
   )
@@ -142,9 +156,9 @@ server <- function(input, output,session) {
       lapply(64:180,function(i){i}),
       lapply(181,function(i){
       if(input$myPosition == "prefers_gk")
-        'True'
-      else
         'False'
+      else
+        'True'
     }),lapply(-1,function(i){i}))
     })
     
@@ -159,6 +173,83 @@ server <- function(input, output,session) {
                          nearest(myPlayerCompare(),weights()))[,comparableStats],
       options = list(
         pageLength = 10, autoWidth = TRUE))
+    
+    output$radarSearchCompare <- renderUI({
+      selectInput(inputId = "radarSearchCompare",
+                  label = "Compare",
+                  choices = (df  %>%
+                                filterPlayers(input$ageRange,
+                                              input$valueRange,
+                                              input$position,
+                                              input$nationality,
+                                              input$league,
+                                              input$club) %>%
+                    nearest(searchPlayerCompare(),weights()))[,2],
+                  selected = c())
+    })
+    
+    output$radarMeCompare <- renderUI({
+      selectInput(inputId = "radarFindMe",
+                  label = "Compare",
+                  choices = (df  %>%
+                               filterPlayers(input$ageRange,
+                                             input$valueRange,
+                                             input$position,
+                                             input$nationality,
+                                             input$league,
+                                             input$club) %>%
+                               nearest(myPlayerCompare(),weights()))[,2],
+                  selected = c())
+    })
+    
+    radarSearchCompare <- reactive({df[df$full_name == input$radarSearchCompare,]})
+    radarFindMe <- reactive({df[df$full_name == input$radarFindMe,]})
+    
+    output$radarSearch <- renderPlot({
+      # Create data: note in High school for Jonathan:
+      data=as.data.frame(((searchPlayerCompare())))[,30:63]
+      data = rbind(data,(radarSearchCompare())[,30:63])
+      # To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
+      data=rbind(rep(99,1) , rep(0,10) , data)
+      
+      # Custom the radarChart !
+      radarchart( data  , axistype=1 ,
+                  
+                  #custom polygon
+                  pcol=c(rgb(0.2,0.5,0.5,0.8),rgb(0.8,0.2,0.2,0.8)) , pfcol=c(rgb(0.2,0.5,0.5,0.4),rgb(0.8,0.2,0.2,0.4)) , plwd=4 ,
+                  
+                  #custom the grid
+                  cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,99,5), cglwd=0.8,
+                  
+                  #custom labels
+                  vlcex=0.65
+      )
+    })
+
+  output$radarFindMe <- renderPlot({
+    # Create data: note in High school for Jonathan:
+    data = as.data.frame((myPlayerCompare()))[,30:63]
+    data2 = radarFindMe()[,30:63]
+    colnames(data2) = colnames(data)
+    rownames(data2) = rownames(data)
+    data = rbind(data,data2)
+    colnames(data) = colnames(df[,30:63])
+    # To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
+    data=rbind(rep(99,1) , rep(0,10) , data)
+    
+    # Custom the radarChart !
+    radarchart( data  , axistype=1 ,
+                
+                #custom polygon
+                pcol=c(rgb(0.2,0.5,0.5,0.8),rgb(0.8,0.2,0.2,0.8)) , pfcol=c(rgb(0.2,0.5,0.5,0.4),rgb(0.8,0.2,0.2,0.4)) , plwd=4 ,
+                
+                #custom the grid
+                cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,99,5), cglwd=0.8,
+                
+                #custom labels
+                vlcex=0.65
+    )
+  })
 }
 
 # Create a Shiny app object
